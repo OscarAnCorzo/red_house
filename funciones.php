e2507e62db4dbfbaf37a91968b61f08d90460566
oscancor117 <oscarcorzog@gmail.com>
2019-08-22 18:35:01 -0500

    arreglo en los encabezados

--- funciones.php
+++ funciones.php
@@ -13,9 +13,7 @@
 
 	function encabezado(){
 		if(isset($_SESSION['correo'])){
-			if($_SESSION['correo'] == 'admin'){
-				encabezadoAdmin();
-			}else  encabezadoCliente();
+            encabezadoCliente();
 		}else encabezado0();
 	}
 
@@ -58,7 +56,7 @@
                     <a class="nav-link letra_enca" href="index.php" >Inicio<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item active ">
-                    <a class="nav-link letra_enca" href="#">Mis Publicaciones</a>
+                    <a class="nav-link letra_enca" href="miPublicacion.php">Mis Publicaciones</a>
                 </li>
                 <li class="nav-item active ">
                     <a class="nav-link letra_enca" href="publicar_oferta.php">Publicar</a>
@@ -67,39 +65,36 @@
         </div>
         <ul class="nav justify-content-end" >
             <li class="nav-item">
-                <a class="nav-link active letra_enca" href="#">Notificaciones</a>
-            </li>
-            <li class="nav-item">
                 <a class="nav-link active letra_enca" href="cerrar_sesion.php">Salir</a>
             </li>
         </ul>
     </nav>';
 	}
 
-	function encabezadoAdmin(){
-		echo '<nav class="navbar navbar-expand-lg navbar-light bg-light col-md-12" style="background:#4CAF50 !important">
-        <a class="navbar-brand" href="#" style="color:WHITE">RED HOUSE</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown" >
-            <ul class="navbar-nav" >
-                <li class="nav-item active" >
-                    <a class="nav-link letra_enca" href="#" >Inicio<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active ">
-                    <a class="nav-link letra_enca" href="#">Publicaciones</a>
-                </li>
-                <li class="nav-item active ">
-                    <a class="nav-link letra_enca" href="#">Estadisticas</a>
-                </li>
-            </ul>
-        </div>
-        <ul class="nav justify-content-end" >
-            <li class="nav-item">
-                <a class="nav-link active letra_enca" href="#">salir</a>
-            </li>
-        </ul>
-    </nav>';
-	}
+	// function encabezadoAdmin(){
+	// 	echo '<nav class="navbar navbar-expand-lg navbar-light bg-light col-md-12" style="background:#4CAF50 !important">
+    //     <a class="navbar-brand" href="#" style="color:WHITE">RED HOUSE</a>
+    //     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    //         <span class="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div class="collapse navbar-collapse" id="navbarNavDropdown" >
+    //         <ul class="navbar-nav" >
+    //             <li class="nav-item active" >
+    //                 <a class="nav-link letra_enca" href="#" >Inicio<span class="sr-only">(current)</span></a>
+    //             </li>
+    //             <li class="nav-item active ">
+    //                 <a class="nav-link letra_enca" href="miPublicacion.php">Publicaciones</a>
+    //             </li>
+    //             <li class="nav-item active ">
+    //                 <a class="nav-link letra_enca" href="#">Estadisticas</a>
+    //             </li>
+    //         </ul>
+    //     </div>
+    //     <ul class="nav justify-content-end" >
+    //         <li class="nav-item">
+    //             <a class="nav-link active letra_enca" href="#">salir</a>
+    //         </li>
+    //     </ul>
+    // </nav>';
+	// }
 ?>
